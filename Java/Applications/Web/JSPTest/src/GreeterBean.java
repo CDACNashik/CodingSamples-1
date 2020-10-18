45e2bf86876660fa9c905739e92f1089a0bf17d5
cdacatcnashik <72591075+cdacatcnashik@users.noreply.github.com>
2020-10-18 17:06:12 +0530

    Add files via upload

---
+++ Java/Applications/Web/JSPTest/src/GreeterBean.java
@@ -0,0 +1,21 @@
+package basic.web.app;
+
+public class GreeterBean implements java.io.Serializable {
+
+	private String person;
+	private String period;
+	private String message;
+
+	public final String getPerson() { return person; }
+	public final void setPerson(String value) { person = value; }
+
+	public final String getPeriod() { return period; }
+	public final void setPeriod(String value) { period = value; }
+
+	public String getGreetingMessage() {
+		if(person == null)
+			return "Welcome Visitor";
+		return String.format("Good %s %s", period, person);
+	}
+}
+
