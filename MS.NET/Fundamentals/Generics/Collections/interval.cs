e429ad8d5d994c2b1fffd937b99c5ca3b824c2b5
cdacatcnashik <72591075+cdacatcnashik@users.noreply.github.com>
2020-10-25 17:04:42 +0530

    Create interval.cs

---
+++ MS.NET/Fundamentals/Generics/Collections/interval.cs
@@ -0,0 +1,34 @@
+partial class Interval : System.IComparable<Interval>
+{
+	private int min, sec;
+
+	public Interval(int m, int s)
+	{
+		min = m + s / 60;
+		sec = s % 60;
+	}
+
+	public int Minutes => min;
+
+	public int Seconds => sec;
+
+	public int Time => 60 * min + sec;
+
+	public override string ToString() => $"{min}:{sec:00}";
+
+	public override int GetHashCode() => min + sec;
+
+	public override bool Equals(object other)
+	{
+		if(other is Interval that)
+		{
+			return (this.min == that.min) && (this.sec == that.sec);
+		}
+		return false;
+	}
+
+	public int CompareTo(Interval that)
+	{
+		return this.Time - that.Time;
+	}
+}
