b1d5c22fa0ab0317d5e0030e43d5d97b650699d2
cdacatcnashik <72591075+cdacatcnashik@users.noreply.github.com>
2020-11-01 11:08:42 +0530

    Create ICart.cs

---
+++ MS.NET/Applications/Distributed/WcfStateTest/Shopping/ICart.cs
@@ -0,0 +1,14 @@
+using System.ServiceModel;
+
+namespace Shopping
+{
+    [ServiceContract(SessionMode = SessionMode.Required)]
+    public interface ICart
+    {
+        [OperationContract]
+        bool AddItem(string name);
+
+        [OperationContract(IsTerminating = true)]
+        double Checkout();
+    }
+}
