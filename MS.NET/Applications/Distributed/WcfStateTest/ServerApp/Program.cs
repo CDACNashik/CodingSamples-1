758f983feee1ad6fa7830df24539df28f20a625f
cdacatcnashik <72591075+cdacatcnashik@users.noreply.github.com>
2020-11-01 11:14:07 +0530

    Add files via upload

--- MS.NET/Applications/Distributed/WcfStateTest/ServerApp/Program.cs
+++ MS.NET/Applications/Distributed/WcfStateTest/ServerApp/Program.cs
@@ -1,28 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.ServiceModel;
 using Shopping;
 
 namespace ServerApp
 {
-    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
-    public class ShopKeeperService : IShopKeeper
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerSession)]
+    public class CartService : ICart
     {
-        public float GetBulkDiscount(int quantity)
-        {
-            return quantity < 3 ? 0 : 15;
-        }
+        private double payment;
 
-        public ItemInfo GetItemInfo(string name)
+        public bool AddItem(string name)
         {
             string[] items = { "cpu", "hdd", "keyboard", "motherboard", "mouse", "ram" };
             double[] prices = { 18000, 4500, 850, 6200, 450, 2400 };
-            int[] stocks = { 25, 38, 150, 25, 120, 65 };
-
             int id = Array.IndexOf(items, name);
-            if (id >= 0)
-                return new ItemInfo { UnitPrice = prices[id], CurrentStock = stocks[id] };
 
-            return null;
+            if(id >= 0)
+            {
+                payment += prices[id];
+                return true;
+            }
+
+            return false;
+        }
+
+        public double Checkout()
+        {
+            return payment;
         }
     }
 
@@ -30,9 +38,8 @@ namespace ServerApp
     {
         static void Main(string[] args)
         {
-            var host = new ServiceHost(typeof(ShopKeeperService));
-            host.AddServiceEndpoint(typeof(IShopKeeper), new NetHttpBinding(), "http://localhost:8056/shopping/shopkeeper");
- 
+            var host = new ServiceHost(typeof(CartService));
+
             host.Open();
             Console.ReadKey();
             host.Close();
