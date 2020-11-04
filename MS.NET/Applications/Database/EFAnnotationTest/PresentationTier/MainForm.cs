03faedcd69a802ba1dd2e55daf40e715f0124c60
cdacatcnashik <72591075+cdacatcnashik@users.noreply.github.com>
2020-11-04 11:34:59 +0530

    Add files via upload

---
+++ MS.NET/Applications/Database/EFAnnotationTest/PresentationTier/MainForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PresentationTier
+{
+    public partial class MainForm : Form
+    {
+        OrderManagerClient client = new OrderManagerClient();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void orderButton_Click(object sender, EventArgs e)
+        {
+            string customerId = customerIdTextBox.Text.ToUpper();
+            int productNo = int.Parse(productNoTextBox.Text);
+            int quantity = (int)quantityNumericUpDown.Value;
+
+            try
+            {
+                int orderNo = client.ProcessOrder(customerId, productNo, quantity);
+                MessageBox.Show($"New order number is {orderNo}", "Order Placed", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch
+            {
+                MessageBox.Show("Order cannot be placed with given inputs", "Order Failed", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+    }
+}
---
+++ MS.NET/Applications/Database/EFAnnotationTest/PresentationTier/OrderManagerClient.cs
@@ -0,0 +1,17 @@
+﻿using Sales;
+using System.ServiceModel;
+
+namespace PresentationTier
+{
+    class OrderManagerClient
+    {
+        public int ProcessOrder(string customerId, int productNo, int quantity)
+        {
+            using(var service = new ChannelFactory<IOrderManager>("OrderManagerTcpEndpoint"))
+            {
+                var client = service.CreateChannel();
+                return client.PlaceOrder(customerId, productNo, quantity);
+            }
+        }
+    }
+}
