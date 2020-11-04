752d49b39c476f018d9d7443038cc7dd8d476105
cdacatcnashik <72591075+cdacatcnashik@users.noreply.github.com>
2020-11-04 11:33:36 +0530

    Add files via upload

---
+++ MS.NET/Applications/Database/EFAnnotationTest/MiddleTier/Program.cs
@@ -0,0 +1,43 @@
+﻿using Sales;
+using System;
+using System.ServiceModel;
+
+namespace MiddleTier
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall)]
+    public class OrderManagerService : IOrderManager
+    {
+        [OperationBehavior(TransactionScopeRequired = true)]
+        public int PlaceOrder(string customerId, int productNo, int quantity)
+        {
+            using(var model = new ShopModel())
+            {
+                Counter ctr = model.Counters.Find("order");
+                OrderEntry order = new OrderEntry 
+                { 
+                    Id = ++ctr.CurrentValue + 1000,
+                    CustomerId = customerId,
+                    ProductNo = productNo,
+                    Quantity = quantity
+                };
+
+                model.Orders.Add(order);
+                model.SaveChanges();
+
+                return order.Id;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new ServiceHost(typeof(OrderManagerService));
+
+            host.Open();
+            Console.ReadKey();
+            host.Close();
+        }
+    }
+}
--- MS.NET/Applications/Database/EFAnnotationTest/MiddleTier/ShopModel.cs
+++ MS.NET/Applications/Database/EFAnnotationTest/MiddleTier/ShopModel.cs
@@ -1,1 +1,41 @@
+﻿using System;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace MiddleTier
+{
+    public class Counter
+    {
+        public string Id { get; set; }
+
+        public int CurrentValue { get; set; }
+    }
+
+    [Table("OrderDetail")]
+    public class OrderEntry
+    {
+        [Column("OrderNo")]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+
+        public DateTime OrderDate { get; set; } = DateTime.Today;
+
+        public string CustomerId { get; set; }
+
+        public int ProductNo { get; set; }
+
+        public int Quantity { get; set; }
+    }
+
+    public class ShopModel : DbContext
+    {
+        public ShopModel() : base("Data Source=.;Initial Catalog=Shop;Integrated Security=True")
+        {
+
+        }
+
+        public DbSet<Counter> Counters { get; set; }
+
+        public DbSet<OrderEntry> Orders { get; set; }
+    }
+}
