2f8367fba930675922dfc04cfb42070d76ae4346
cdacatcnashik <72591075+cdacatcnashik@users.noreply.github.com>
2020-11-06 17:12:57 +0530

    Add files via upload

---
+++ MS.NET/Applications/Web/WebApiTest/RichClientApp/ClientModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RichClientApp
+{
+    [DataContract]
+    class ProductInfo
+    {
+        [DataMember(Name = "id")]
+        public int Id { get; set; }
+
+        [DataMember(Name = "price")]
+        public decimal Price { get; set; }
+
+        [DataMember(Name = "stock")]
+        public int Stock { get; set; }
+    }
+
+    [ServiceContract]
+    interface IProductClient
+    {
+        [OperationContract]
+        [WebGet(UriTemplate = "/api/products/", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+        IEnumerable<ProductInfo> GetProducts();
+
+        [OperationContract]
+        [WebInvoke(Method = "PUT", UriTemplate = "/api/products/", RequestFormat = WebMessageFormat.Json)]
+        void PutProduct(ProductInfo input);
+    }
+
+    class ClientModel
+    {
+        Uri serviceUri = new Uri(Properties.Settings.Default.ServiceAddress);
+
+        public IEnumerable<ProductInfo> GetProducts()
+        {
+            using(var service = new WebChannelFactory<IProductClient>(serviceUri))
+            {
+                var client = service.CreateChannel();
+                return client.GetProducts();
+            }
+        }
+
+        public void PutProduct(ProductInfo input)
+        {
+            using (var service = new WebChannelFactory<IProductClient>(serviceUri))
+            {
+                var client = service.CreateChannel();
+                client.PutProduct(input);
+            }
+        }
+    }
+}
---
+++ MS.NET/Applications/Web/WebApiTest/RichClientApp/ClientViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace RichClientApp
+{
+    class ClientViewModel : MvvmSupport.BindableObjectBase
+    {
+        ClientModel model = new ClientModel();
+
+        public IEnumerable<ProductInfo> Products => model.GetProducts();
+
+        public ProductInfo CurrentProduct
+        {
+            get => _CurrentProduct;
+            set => SetProperty(ref _CurrentProduct, value);
+        }
+        private ProductInfo _CurrentProduct;
+
+        public string Message
+        {
+            get => _Message;
+            set => SetProperty(ref _Message, value);
+        }
+        private string _Message;
+
+        public ICommand Save => DispatchCommand();
+        private bool Save_CanExecute(object parameter) => _CurrentProduct != null;
+        private void Save_Execute(object parameter)
+        {
+            try
+            {
+                model.PutProduct(_CurrentProduct);
+                Message = "Product updated successfully";
+            }
+            catch
+            {
+                Message = "Product update failed";
+            }
+        }
+
+
+
+    }
+}
